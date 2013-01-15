423e8a6eeb45e523bdad490e8240e997afb9b2d9
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-01-15 13:23:42 +0200

    Initial commit, add calculator server.

---
+++ server.php
@@ -0,0 +1,33 @@
+<?php
+// ini_set('display_errors', 'on');
+// error_reporting(E_ALL);
+
+function error($msg) {
+    print(json_encode(array('error' => $msg)));
+    exit();
+}
+
+function result($res) {
+    print(json_encode(array('result' => $res)));
+    exit();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+header("Content-type: text/json\r\n");
+
+if(!isset($_GET['arg1']) || !isset($_GET['arg2']) || !isset($_GET['op']))
+    error('Invalid GET params.');
+
+$arg1 = $_GET['arg1'];
+$arg2 = $_GET['arg2'];
+
+if(!is_numeric($arg1) || !is_numeric($arg2))
+    error('Non-numeric args');
+
+switch($_GET['op']) {
+    case 'add': result($arg1 + $arg2); break;
+    case 'sub': result($arg1 - $arg2); break;
+    case 'mul': result($arg1 * $arg2); break;
+    case 'div': result($arg1 / $arg2); break;
+    default: error('Invalid operator'); break;
+}
