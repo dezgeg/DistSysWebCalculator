9ba9e9c911610be1a980edf996a9aba6493fac39
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-01-17 22:02:37 +0200

    Split client0.js to common.js

--- client0.js
+++ client0.js
@@ -1,28 +1,4 @@
 jQuery(function($) {
-    var history;
-
-    function addToHistoryView(calculation) {
-        var list = $('#calculationResults');
-        var entry = $('<div>');
-        entry.text("" + calculation.arg1 + " " + calculation.op +
-            " " + calculation.arg2 + " = " + calculation.result);
-        list.append(entry);
-    }
-
-    function calculate(params) {
-        var result = $.ajax('server.php', { async: false, data: params });
-        if(result.status != 200)
-            throw new Error('Network error: ' + result.status +
-                ' - ' + result.statusText);
-        var resultJSON = JSON.parse(result.responseText);
-
-        var calculation = {};
-        for(k in params)
-            calculation[k] = params[k];
-        calculation.result = resultJSON.result;
-        return calculation;
-    }
-
     function onFormSubmit(e) {
         e.preventDefault();
         var form = this;
@@ -36,16 +12,12 @@ jQuery(function($) {
             return false;
         }
 
-        var calc = calculate(params);
-        history.push(calc);
-        addToHistoryView(calc);
-        window.localStorage.calcHistory = JSON.stringify(history);
+        doCalculation(params);
     }
 
     function init() {
-        history = JSON.parse(window.localStorage.calcHistory || '[]');
-        $.each(history, function(i, calc) { addToHistoryView(calc); });
         $('#mainForm').on('submit', onFormSubmit);
+        initHistory();
     }
     init();
 });
---
+++ common.js
@@ -0,0 +1,37 @@
+var history;
+
+function addToHistoryView(calculation) {
+    var list = $('#calculationResults');
+    var entry = $('<div>');
+    entry.text("" + calculation.arg1 + " " + calculation.op +
+        " " + calculation.arg2 + " = " + calculation.result);
+    list.append(entry);
+}
+
+function initHistory() {
+    history = JSON.parse(window.localStorage.calcHistory || '[]');
+    $.each(history, function(i, calc) { addToHistoryView(calc); });
+}
+
+function calculateOnServer(params) {
+    var result = $.ajax('server.php', { async: false, data: params });
+    if(result.status != 200)
+        throw new Error('Network error: ' + result.status +
+            ' - ' + result.statusText);
+    var resultJSON = JSON.parse(result.responseText);
+
+    var calculation = {};
+    for(k in params)
+        calculation[k] = params[k];
+    calculation.result = resultJSON.result;
+    return calculation;
+}
+
+function doCalculation(params) {
+    var calc = calculateOnServer(params);
+    history.push(calc);
+    addToHistoryView(calc);
+    window.localStorage.calcHistory = JSON.stringify(history);
+    return calc;
+}
+
