e4a64f6f9bc22514b2fbdb15d7b1cfac56ae4520
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-02-17 23:40:20 +0200

    Gnuplot plotting on PHP side.

--- server.php
+++ server.php
@@ -40,21 +40,44 @@ function really_divide($arg1, $arg2) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-header("Content-type: text/json\r\n");
+if(isset($_GET['plot'])) {
+    $func = $_GET['plot'];
+    if (preg_match('#^[-+*/x0-9.eEsincos( )]+$#', $func) !== 1)
+        error('Plotted function contains invalid characters');
+    if (trim($func) === '')
+        error('Empty expression');
 
-if(!isset($_GET['arg1']) || !isset($_GET['arg2']) || !isset($_GET['op']))
-    error('Invalid GET params.');
+    $gnuplot_expr = escapeshellarg('set term png; plot ' . $func);
+    // validate
+    $exit_status = 1;
+    $output = array();
+    exec('gnuplot 2>&1 1>/dev/null -e ' . $gnuplot_expr, $output, $exit_status);
+    if ($exit_status !== 0) {
+        header("HTTP/1.1 403 Bad Request\r\n");
+        header("Content-type: text/json\r\n");
+        error('Server-side plotting failed');
+    }
 
-$arg1 = really_to_float($_GET['arg1']);
-$arg2 = really_to_float($_GET['arg2']);
+    header("Content-type: image/png\r\n");
+    passthru('gnuplot -e ' . $gnuplot_expr);
 
-if ($arg1 === NULL || $arg2  === NULL)
-    error('Non-numeric args');
+} else {
 
-switch($_GET['op']) {
-    case '+': result($arg1 + $arg2); break;
-    case '-': result($arg1 - $arg2); break;
-    case '*': result($arg1 * $arg2); break;
-    case '/': result(really_divide($arg1, $arg2)); break;
-    default: error('Invalid operator.'); break;
+    header("Content-type: text/json\r\n");
+    if(!isset($_GET['arg1']) || !isset($_GET['arg2']) || !isset($_GET['op']))
+        error('Invalid GET params.');
+
+    $arg1 = really_to_float($_GET['arg1']);
+    $arg2 = really_to_float($_GET['arg2']);
+
+    if ($arg1 === NULL || $arg2  === NULL)
+        error('Non-numeric args');
+
+    switch($_GET['op']) {
+        case '+': result($arg1 + $arg2); break;
+        case '-': result($arg1 - $arg2); break;
+        case '*': result($arg1 * $arg2); break;
+        case '/': result(really_divide($arg1, $arg2)); break;
+        default: error('Invalid operator.'); break;
+    }
 }
